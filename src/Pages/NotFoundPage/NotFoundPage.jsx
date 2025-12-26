02f82931eee1a73ba28dc603b76a134ee3c040bb
SOFIUL AZAM <shsofiul@gmail.com>
2025-12-26 21:24:29 +0600

    page not found option added

--- src/Pages/NotFoundPage/NotFoundPage.jsx
+++ src/Pages/NotFoundPage/NotFoundPage.jsx
@@ -5,8 +5,14 @@ import { Link } from 'react-router';
 const NotFoundPage = () => {
     return (
         
-            <div>
-                
+            <div className="flex flex-col items-center mt-12">
+              <img className="w-64 h-64 mb-4"  src={error} alt="" />
+              <h1 className="text-2xl font-bold">Oops, page not found!</h1>
+              <p>The page you are looking for is not available.</p>
+              <Link to='/'
+                              onClick={() =>{window.scrollTo(0,0)}}>
+                              <button className='mt-4 text-xl bg-gradient-to-r from-[#632EE3] to-[#9F62F2] p-2 text-white rounded-sm'>Go Back!</button>
+                    </Link>
             </div>
     );
 };
